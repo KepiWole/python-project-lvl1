2a804fe2c706a3d915a82487fe05c4a096acf8c1
etogit <sturik@bk.ru>
2022-10-19 16:00:26 +0300

    add .

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
 greeting = "Welcome to the Brain Games!"
 def greet(greeting):
   print(greeting)
+  welcome_user()
 def main():
   greet(greeting)
 if __name__ == '__main__':
