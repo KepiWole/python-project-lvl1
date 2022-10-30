cdd62665bd815e6bb3c8f42076dd22e656f48eee
etogit <sturik@bk.ru>
2022-10-30 16:06:15 +0300

    add *

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,29 +2,31 @@
 from random import randint
 from brain_games.cli import welcome_user
 from brain_games.scripts.brain_games import main
+
+
 main()
+
+
 def rand():
-  name = welcome_user()[1]
-  print('Answer "yes" if the number is even, otherwise answer "no".')
-  good_result = 0
-  while True:
-    result = randint(1, 10)
-    print(f'Question: {result}')
-    text = input('Your answer: ').lower()
-    if result % 2 == 0 and text == 'yes':
-      good_result += 1
-      print('Correct!')
-      if good_result == 3:
-       return print(f'Congratulations, {name}!')
-    elif result % 2 != 0 and text == 'no':
-      good_result += 1
-      print('Correct!')
-      if good_result == 3:
-        return print(f'Congratulations, {name}!')
-    elif result % 2 != 0 and text == 'yes':
-      return print(f"Let's try again, {name}!")
-    elif result % 2 == 0 and text == 'no':
-      return print(f"Let's try again, {name}!")
-    elif text !='yes' or 'no':
-      return print(f"Let's try again, {name}!")
+    name = welcome_user()[1]
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    good_result = 0
+    while True:
+        result = randint(1, 10)
+        print(f'Question: {result}')
+        text = input('Your answer: ').lower()
+        if result % 2 == 0 and text == 'yes':
+            good_result += 1
+            print('Correct!')
+            if good_result == 3:
+                return print(f'Congratulations, {name}!')
+        elif result % 2 != 0 and text == 'no':
+            good_result += 1
+            print('Correct!')
+            if good_result == 3:
+                return print(f'Congratulations, {name}!')
+        else:
+            return print(f"Let's try again, {name}!")
+
+
 rand()
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
-
-
-from brain_games.cli import welcome_user
 greeting = "Welcome to the Brain Games!"
 
 
-
 def main():
-	print(greeting)
+    print(greeting)
 
 
 if __name__ == '__main__':
-	main()
+    main()
