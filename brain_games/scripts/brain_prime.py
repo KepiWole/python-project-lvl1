6a76cc43a24ba9e361d6b3c5de21fe5567faa3ce
etogit <sturik@bk.ru>
2022-11-01 16:39:46 +0300

    add *

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 from random import randint
 from brain_games.cli import welcome_user
 from brain_games.scripts.brain_games import main
@@ -7,35 +6,35 @@ from brain_games.scripts.brain_games import main
 main()
 
 
-def rand():
+def prime():
     name = welcome_user()[1]
-    print('Answer "yes" if the number is even, otherwise answer "no".')
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
     good_result = 0
     while True:
-        result = randint(1, 10)
+        result = randint(1, 20)
         print(f'Question: {result}')
         text = input('Your answer: ').lower()
-        if result % 2 == 0 and text == 'yes':
+        if result < 10 and text == 'yes':
             good_result += 1
             print('Correct!')
             if good_result == 3:
-                return print(f'Congratulations, {name}!')
-        elif result % 2 != 0 and text == 'no':
+                return print(f'Congratulations, {name}')
+        elif result > 10 and text == 'no':
             good_result += 1
             print('Correct!')
             if good_result == 3:
-                return print(f'Congratulations, {name}!')
+                return print(f'Congratulations, {name}')
         else:
             text2 = ''
-            if result % 2 == 0:
+            if result <= 9:
                 text2 += 'yes'
-            elif result % 2 != 0:
+            elif result >= 10:
                 text2 += 'no'
             return print(f'\'{text}\' is wrong answer ;(. Correct answer was \'{text2}\'.\nLet\'s try again, {name}')
 
 
 def main():
-    rand()
+    prime()
 
 
 if __name__ == '__main__':
